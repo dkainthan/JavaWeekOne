b3f075c58f6b85da736360c0d10a808a503634be
dkainthan <dkainthan@gmail.com>
2025-02-11 10:05:53 -0500

    wowoowooww

---
+++ Comments.java
@@ -0,0 +1,16 @@
+/**************************
+ * Project:Demo for Comments 
+ * Programer: Diua Kainthan 
+ * Date: Febuary 11th, 2025
+ * Program Name: Comments.java
+ **************************/
+
+public class Comments {
+    public static void main(String[]args) 
+    {
+        System.out.println("Hi"); //message to user 
+        System.out.println("LabOne"); //assignment 
+    
+    }
+    
+}
