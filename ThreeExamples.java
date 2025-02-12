8b05070c1e7c104690fdb0ee44cb59f214b5f0d4
dkainthan <dkainthan@gmail.com>
2025-02-12 10:09:57 -0500

    ooo lal a

--- ThreeExamples.java
+++ ThreeExamples.java
@@ -1,6 +1,16 @@
 public class ThreeExamples {
     public static void main(String[]args) 
     {
-        System.out.println("\n\thello\\\'"); 
+        //System.out.println("\n\thello\\\'"); 
+        System.out.println("\tDog Show Winners");
+        System.out.println("Place:\t1\t2\t3\t4\t5\t6"); 
+        System.out.println("Dog#:\t76\t32\t18\t47\t11\t29\n");
+
+
+        System.out.println("Q\"yz89\"Ja4\"tp\n"); 
+
+        System.out.println("\tAnderson\t3452\n\tBoyton\t\t5344\n\tCarrington\t3677\n\tHouston\t\t5988\n\tMcDougal\t5233\n\tRobertson\t3623\n\tStryker\t\t5112");
+       
+
     }
 }
